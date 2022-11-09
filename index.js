fb786d00b58a6968e6e051f4af79e3249e469ab6
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-09 17:22:54 -0300

    criando conexão do banco mongo

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const SERVER_PORT = 5500
 
 const app = express()
 
+mongoose.connect("mongodb://localhost:27017/DB_JWT", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => { console.log("DB connection Success!") })
+
 app.use(cors({
     origin: ["http://localhost:3000"],
     method: ["GET", "POST"],
