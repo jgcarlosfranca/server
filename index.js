a3c85c4b9ea80a6646360db9f8175a10f17895fa
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-09 16:35:13 -0300

    add cors e json na aplicação express

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors")
 
 const SERVER_PORT = 5500
 
 const app = express()
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET", "POST"],
+    credentials: true
+}))
+
+app.use(express.json())
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server UP na porta ${SERVER_PORT}`)
