47294421a1a70ae7117112ac4946cdb4e78e1eb0
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-13 14:46:23 -0300

    colocando server na  constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 const SECRET_PHRASE = "Hupão é muito melhor que a Raiden Shogun"
 const MAX_TIME_EXPIRED_TOKEN = 3 * 24 * 60 * 60
+const SERVER_PORT = 5500
 
 
-module.exports = { SECRET_PHRASE, MAX_TIME_EXPIRED_TOKEN }
+module.exports = { SECRET_PHRASE, MAX_TIME_EXPIRED_TOKEN, SERVER_PORT }
